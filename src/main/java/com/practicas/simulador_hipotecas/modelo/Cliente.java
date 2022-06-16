d32b728db66ed17fa57b500b1f7ce17b63525a2a
pablopa1 <pprieto@serbatic.es>
2022-06-16 10:07:56 +0200

    Añadido JPA

--- src/main/java/com/practicas/simulador_hipotecas/modelo/Cliente.java
+++ src/main/java/com/practicas/simulador_hipotecas/modelo/Cliente.java
@@ -2,13 +2,28 @@ package com.practicas.simulador_hipotecas.modelo;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 /**
  * @author pprieto
  *
  */
+@Entity
+@Table(name = "clientes")
 public class Cliente {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	
+	@ManyToOne
+	private Rol rol;
+	
 	private String email;
 	private String clave;
 	private String nombre;
--- src/main/java/com/practicas/simulador_hipotecas/modelo/Rol.java
+++ src/main/java/com/practicas/simulador_hipotecas/modelo/Rol.java
@@ -1,9 +1,24 @@
 package com.practicas.simulador_hipotecas.modelo;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "roles")
 public class Rol {
 
-
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(columnDefinition = "ENUM ('CLIENTE')")
 	private RolTipo rol;
 
 	public Rol() {
