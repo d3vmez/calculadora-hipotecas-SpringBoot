ec16c8f2e5531b89253f5cf67786d9b481f82d53
pablopa1 <pprieto@serbatic.es>
2022-06-16 09:39:02 +0200

    Cliente POJO

---
+++ src/main/java/com/practicas/simulador_hipotecas/modelo/Cliente.java
@@ -0,0 +1,141 @@
+package com.practicas.simulador_hipotecas.modelo;
+
+import java.util.Date;
+
+/**
+ * @author pprieto
+ *
+ */
+public class Cliente {
+
+	private int id;
+	private String email;
+	private String clave;
+	private String nombre;
+	private String apellido1;
+	private String apellido2;
+	private String direccion;
+	private String provincia;
+	private String localidad;
+	private String telefono;
+	private String dni;
+	private Date fechaAlta;
+	private Date fechaBaja;
+	private String imagen;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getClave() {
+		return clave;
+	}
+	public void setClave(String clave) {
+		this.clave = clave;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido1() {
+		return apellido1;
+	}
+	public void setApellido1(String apellido1) {
+		this.apellido1 = apellido1;
+	}
+	public String getApellido2() {
+		return apellido2;
+	}
+	public void setApellido2(String apellido2) {
+		this.apellido2 = apellido2;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public String getProvincia() {
+		return provincia;
+	}
+	public void setProvincia(String provincia) {
+		this.provincia = provincia;
+	}
+	public String getLocalidad() {
+		return localidad;
+	}
+	public void setLocalidad(String localidad) {
+		this.localidad = localidad;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	public String getDni() {
+		return dni;
+	}
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	public Date getFechaAlta() {
+		return fechaAlta;
+	}
+	public void setFechaAlta(Date fechaAlta) {
+		this.fechaAlta = fechaAlta;
+	}
+	public Date getFechaBaja() {
+		return fechaBaja;
+	}
+	public void setFechaBaja(Date fechaBaja) {
+		this.fechaBaja = fechaBaja;
+	}
+	public String getImagen() {
+		return imagen;
+	}
+	public void setImagen(String imagen) {
+		this.imagen = imagen;
+	}
+	public Cliente(int id, String email, String clave, String nombre, String apellido1, String apellido2,
+			String direccion, String provincia, String localidad, String telefono, String dni, Date fechaAlta,
+			Date fechaBaja, String imagen) {
+		super();
+		this.id = id;
+		this.email = email;
+		this.clave = clave;
+		this.nombre = nombre;
+		this.apellido1 = apellido1;
+		this.apellido2 = apellido2;
+		this.direccion = direccion;
+		this.provincia = provincia;
+		this.localidad = localidad;
+		this.telefono = telefono;
+		this.dni = dni;
+		this.fechaAlta = fechaAlta;
+		this.fechaBaja = fechaBaja;
+		this.imagen = imagen;
+	}
+	public Cliente() {
+		super();
+	}
+	
+	@Override
+	public String toString() {
+		return "Cliente [id=" + id + ", email=" + email + ", clave=" + clave + ", nombre=" + nombre + ", apellido1="
+				+ apellido1 + ", apellido2=" + apellido2 + ", direccion=" + direccion + ", provincia=" + provincia
+				+ ", localidad=" + localidad + ", telefono=" + telefono + ", dni=" + dni + ", fechaAlta=" + fechaAlta
+				+ ", fechaBaja=" + fechaBaja + ", imagen=" + imagen + "]";
+	}
+	
+	
+}
