597a6929ae6eecd8f8b8b7863978ee135b9ffcc2
VASS\mgomezgarrote <mgomezgarrote@serbatic.es>
2022-06-20 11:47:07 +0200

    Añadido clase Simulacion

---
+++ src/main/java/com/practicas/simulador_hipotecas/modelo/Simulacion.java
@@ -0,0 +1,54 @@
+package com.practicas.simulador_hipotecas.modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Simulacion {
+	
+	private List<Hipoteca> hipotecasVariables = new ArrayList<>();
+	private Hipoteca hipotecaFija;
+	private int nSimulaciones;
+	
+	public Simulacion() {
+		super();
+		
+	}
+
+	public Simulacion(List<Hipoteca> hipotecasVariables, Hipoteca hipotecaFija, int nSimulaciones) {
+		super();
+		this.hipotecasVariables = hipotecasVariables;
+		this.hipotecaFija = hipotecaFija;
+		this.nSimulaciones = nSimulaciones;
+	}
+
+	public List<Hipoteca> getHipotecasVariables() {
+		return hipotecasVariables;
+	}
+
+	public void setHipotecasVariables(List<Hipoteca> hipotecasVariables) {
+		this.hipotecasVariables = hipotecasVariables;
+	}
+
+	public Hipoteca getHipotecaFija() {
+		return hipotecaFija;
+	}
+
+	public void setHipotecaFija(Hipoteca hipotecaFija) {
+		this.hipotecaFija = hipotecaFija;
+	}
+
+	public int getnSimulaciones() {
+		return nSimulaciones;
+	}
+
+	public void setnSimulaciones(int nSimulaciones) {
+		this.nSimulaciones = nSimulaciones;
+	}
+
+	@Override
+	public String toString() {
+		return "Simulacion [hipotecasVariables=" + hipotecasVariables + ", hipotecaFija=" + hipotecaFija
+				+ ", nSimulaciones=" + nSimulaciones + "]";
+	}
+	
+}
